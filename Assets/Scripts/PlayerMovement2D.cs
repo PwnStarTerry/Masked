b3ad87f3365c368e6d488de00a1fd3840b6c3ecf
Tater-Ctrl <71195494+Tater-Ctrl@users.noreply.github.com>
2021-03-26 20:35:32 +0100

    Movement and Tilemap
    Basic movement script with a tilemap and sprite sheet for testing purposes.

---
+++ Assets/Scripts/PlayerMovement2D.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement2D : MonoBehaviour
+{
+    [SerializeField] private int speed = 6;
+
+    private Vector2 moveDir;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        moveDir = (transform.right * Input.GetAxisRaw("Horizontal") + transform.up * Input.GetAxisRaw("Vertical")).normalized;
+    }
+
+    void FixedUpdate() {
+        rb.velocity = moveDir * speed;
+    }
+}
